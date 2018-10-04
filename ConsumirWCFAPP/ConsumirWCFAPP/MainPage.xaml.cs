c5a6d167773733501fda18d2933fca0c80e5d731
IvanPan7 <ivanpan96@gmail.com>
2018-10-04 15:57:05 -0600

    first commit without views

---
+++ ConsumirWCFAPP/ConsumirWCFAPP/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ActividadWCF;
+using System.ServiceModel;
+
+namespace ConsumirWCFAPP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetData();
+        }
+        private void GetData()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress EndPoint = new EndpointAddress("http://192.168.60.157:88/booking");
+            //Reservation r = new Reservation
+            //{
+            //    HotelName = "HotelA"
+            //};
+            //HotelBookingServiceClient Cliente = new HotelBookingServiceClient(binding, EndPoint);
+            //BookingResponse Result = Cliente.BookHotel(r);
+            //lblResult.Text = Result.BookingReference.ToString();
+        }
+    }
+}
